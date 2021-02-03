37a2c5831a1a5860df67191e17abb47833695143
Epic91 <hernandeze791@gmail.com>
2021-02-02 21:47:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+//setting up an event listener
+
+document.addEventListener("DOMContentLoaded", function () {
+    detectDOM();
+});
+
+function detectDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!";
+}
